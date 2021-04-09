23d5d72e24a6b06c2dcd78724d9762c2e9810563
Nils Peritore <nils.peritore@epitech.eu>
2021-04-09 17:11:50 +0200

    fix

---
+++ lib/entity/order_entity.d.ts
@@ -0,0 +1,15 @@
+export interface OrderEntity {
+    amount_received: number;
+    items: any[];
+    left_to_pay: number;
+    mission_ids: string[];
+    payment_intents: [];
+    price: {
+        discount: number;
+        price_ht: number;
+        total_ht: number;
+        tva: number;
+    };
+    status: string;
+    _id: string;
+}
