3720fe0da12e315b37e37c2d87d9513bb11e1cd2
Théo Campillo <theo.campillo@epitech.eu>
2021-05-06 17:15:29 +0200

    feat: parcel refuse reason trans

---
+++ src/enums/parcel_refuse_reason_enum.ts
@@ -0,0 +1,14 @@
+export enum ParcelRefuseReasonEnum {
+    bad_address = "bad_address",
+    customer_unreachable = "customer_unreachable",
+    damaged = "damaged",
+    customer_request = "customer_request",
+    dhl_request = "dhl_request",
+    delivery_failure = "delivery_failure",
+    delivery_too_far = "delivery_too_far",
+    delivery_volume_too_high = "delivery_volume_too_high",
+    delivery_weight_too_high = "delivery_weight_too_high",
+    other = "other",
+    out_of_zone = "out_of_zone",
+    provider_request = "provider_request",
+}
