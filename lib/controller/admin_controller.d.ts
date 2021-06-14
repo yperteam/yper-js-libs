789276f4462dc7078a7f597e8acd581b0ea08e45
Serre Thibault <33985678+SerreThibault@users.noreply.github.com>
2021-06-14 14:36:59 +0200

    Feat/add admin controller (#25)
    Feat/add admin controller (#25)

---
+++ lib/controller/admin_controller.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="jquery" />
+/// <reference types="jquery.blockui" />
+/// <reference types="jqueryui" />
+/// <reference types="bootstrap" />
+import { AbstractLib } from "../libs/abstract_lib";
+export default class AdminController extends AbstractLib {
+    /**
+     *
+     * @param $elem
+     */
+    constructor($elem?: JQuery<HTMLElement> | null);
+    /**
+     * Send Message to user(s)
+     */
+    postMessage(): Promise<any>;
+}
