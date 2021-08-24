4f35ea72b04cdee8789b87a7e401bb86021df279
Serre Thibault <serrethibault@MacBook-Pro-Thibault-Serre.local>
2021-08-24 09:21:05 +0200

    feat - Update Delivry Parcel Failure Reason Enum

--- lib/enums/delivery_parcel_failure_reason_enum.d.ts
+++ lib/enums/delivery_parcel_failure_reason_enum.d.ts
@@ -16,6 +16,5 @@ export declare enum DeliveryParcelFailureReasonEnum {
     deliverer_workload_too_high = "deliverer_workload_too_high",
     out_of_business_hours = "out_of_business_hours",
     hub_customer_handover = "hub_customer_handover",
-    delivery_postponed = "delivery_postponed",
-    out_of_zone = "out_of_zone"
+    delivery_postponed = "delivery_postponed"
 }
