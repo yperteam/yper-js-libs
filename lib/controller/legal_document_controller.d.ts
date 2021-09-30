db646538755b398002caf3c57f551f1f7cbe755d
Serre Thibault <serrethibault@MacBook-Pro-Thibault-Serre.local>
2021-09-30 12:16:52 +0200

    generic commit

---
+++ lib/controller/legal_document_controller.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="jquery" />
+/// <reference types="jquery.blockui" />
+/// <reference types="jqueryui" />
+/// <reference types="bootstrap" />
+import { AbstractLib } from "../libs/abstract_lib";
+export default class LegalDocumentController extends AbstractLib {
+    /**
+     *
+     * @param $elem
+     */
+    constructor($elem?: JQuery<HTMLElement> | null);
+    /**
+     * Get Availability Color
+     */
+    getAvailabilityColor(): Promise<any>;
+}
